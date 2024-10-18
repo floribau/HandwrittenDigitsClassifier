9490ab59b6f159aa445d4b41c2d1d1a19c311932
Florian Bauer <fpb255@gmx.de>
2024-10-18 12:08:45 +0200

    remove exception handling from main.py for debugging

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
+
 model = tf.keras.models.load_model('handwritten-digits-nn.keras')
 
 image_number = 1
 while os.path.isfile(f"digits/digit{image_number}.png"):
-    try:
-        img = cv2.imread(f"digits/digit{image_number}.png")[:, :, 0]
-        img = np.invert(np.array([img]))
-        prediction = model.predict(img)
-        print(f"The digit in digit{image_number}.png is probably a {np.argmax(prediction)}")
-    except:
-        print("Error!")
-    finally:
-        image_number += 1
+    img = cv2.imread(f"digits/digit{image_number}.png")[:, :, 0]
+    img = np.invert(np.array([img]))
+    prediction = model.predict(img)
+    print(f"The digit in digit{image_number}.png is probably a {np.argmax(prediction)}")
+    image_number += 1
